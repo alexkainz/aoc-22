6a2ffcf85d4fbb451f6c014b3efa6168940925fc
akainz <alexander.kainz@dataspot.at>
2022-12-04 20:06:46 +0100

    feat: solve day3

---
+++ src/day3.rs
@@ -0,0 +1,67 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+pub struct Day3;
+use crate::puzzle::Puzzle;
+
+const CAPITAL_A: i32 = 65;
+const SMALL_A: i32 = 97;
+
+fn priority(codepoint: i32) -> i32 {
+    if codepoint >= SMALL_A { codepoint - SMALL_A + 1 } else { codepoint - CAPITAL_A + 27 }
+}
+
+impl Puzzle for Day3 {
+    fn info(&self) -> (i8, String) { (3, String::from("Rucksack Reorganization")) }
+
+    fn solve1(&self, path: &Path) -> i32 {
+        let file = File::open(path).unwrap();
+        let buf = BufReader::new(file);
+
+        let mut sum_priorities = 0;
+
+        for line in buf.lines() {
+            let line = line.unwrap();
+            let (first, second) = line.split_at(line.len() / 2);
+            let first = String::from(first);
+            let common = second.chars()
+                .find(|c| first.contains(*c))
+                .unwrap() as i32;
+
+            sum_priorities += priority(common);
+        }
+
+        return sum_priorities;
+    }
+
+    fn expected1(&self) -> [i32; 2] { [157, 8153] }
+
+    fn solve2(&self, path: &Path) -> i32 {
+        let file = File::open(path).unwrap();
+        let buf = BufReader::new(file);
+
+        let mut sum_priorities = 0;
+        let mut group: [String; 2] = [ String::new(), String::new() ];
+
+        for (i, line) in buf.lines().enumerate() {
+            let line = line.unwrap();
+            let elf_in_group = i % 3;
+
+            if elf_in_group != 2 {
+                group[elf_in_group] = line;
+                continue;
+            }
+
+            let common = line.chars()
+                .find(|c| group[0].contains(*c) && group[1].contains(*c))
+                .unwrap() as i32;
+
+            sum_priorities += priority(common);
+        }
+
+        return sum_priorities;
+    }
+
+    fn expected2(&self) -> [i32; 2] { [70, 2342] }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod puzzle;
 mod day1;
 mod day2;
+mod day3;
 
 use std::path::Path;
+use std::time::Instant;
+use colored::Colorize;
 
 fn solve(day: Box<dyn puzzle::Puzzle>) {
     let (nr, name) = day.info();
@@ -14,28 +17,39 @@ fn solve(day: Box<dyn puzzle::Puzzle>) {
     let input = Path::new(&input);
 
     let puzzle_name = format!("Day {:02}: {}", nr, name);
-    println!("--- {} ---", puzzle_name);
+    println!("--- {} ---", puzzle_name.bold());
 
     let [expected, answer] = day.expected1();
     assert_eq!(day.solve1(example), expected, "{} (example)", puzzle_name);
-    assert_eq!(day.solve1(input), answer, "{} (input)", puzzle_name);
-    println!("    Answer: {}", answer);
+
+    let start = Instant::now();
+    let expected = day.solve1(input);
+    let duration = start.elapsed();
+
+    assert_eq!(expected, answer, "{} (input)", puzzle_name);
+    println!("    Answer: {} [{:?}]", answer, duration);
 
     let puzzle_name = format!("Day {:02}: Part Two", nr);
-    println!("--- {} ---", puzzle_name);
+    println!("--- {} ---", puzzle_name.bold());
 
     let [expected, answer] = day.expected2();
     assert_eq!(day.solve2(example), expected, "{} (example)", puzzle_name);
-    assert_eq!(day.solve2(input), answer, "{} (input)", puzzle_name);
-    println!("    Answer: {}", answer);
+
+    let start = Instant::now();
+    let expected = day.solve2(input);
+    let duration = start.elapsed();
+
+    assert_eq!(expected, answer, "{} (input)", puzzle_name);
+    println!("    Answer: {} [{:?}]", answer, duration);
 }
 
 fn main() {
-    println!("\nAdvent of Code\n       λy.2022\n");
+    println!("\n{}\n       {}\n", "Advent of Code".green(), "λy.2022".green());
 
-    let days: [Box<dyn puzzle::Puzzle>; 2] = [
+    let days: [Box<dyn puzzle::Puzzle>; 3] = [
         Box::new(day1::Day1 {}),
-        Box::new(day2::Day2 {})
+        Box::new(day2::Day2 {}),
+        Box::new(day3::Day3 {})
     ];
 
     for day in days {
